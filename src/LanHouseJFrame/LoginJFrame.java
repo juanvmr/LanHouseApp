5b2c45a70e5ca7c75f98089ee4b34fe783408a9f
Juan V. May da Rosa <juan97victor@hotmail.com>
2018-05-04 20:25:49 -0300

    Alteração e adição de InternalFrames.

--- src/LanHouseJFrame/LoginJFrame.java
+++ src/LanHouseJFrame/LoginJFrame.java
@@ -1,23 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package LanHouseJFrame;
 
+import LanHouseJFrame.MainJFrame;
 import java.awt.AWTEvent;
 import java.awt.event.ActionEvent;
 
-/**
- *
- * @author juanvmr
- */
 public class LoginJFrame extends javax.swing.JFrame {
 
-    /**
-     * Creates new form LoginJFrame
-     */
+
     public LoginJFrame() {
         initComponents();
+        //this.add(M)
+        //this.dispatchEvent(e);
     }
 
-    @SuppressWarnings("unchecked")
 
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
@@ -29,7 +32,7 @@ public class LoginJFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 12));
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jLabel1.setText("Login");
 
         jLabel2.setText("Código:");
@@ -37,6 +40,11 @@ public class LoginJFrame extends javax.swing.JFrame {
         jLabel3.setText("Senha:");
 
         OKJButton.setText("OK");
+        OKJButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                OKJButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -79,19 +87,31 @@ public class LoginJFrame extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>                        
+    }// </editor-fold>//GEN-END:initComponents
 
-    private void OKJButtonActionPerformed(java.awt.event.ActionEvent evt) {                                          
+    private void OKJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OKJButtonActionPerformed
         if(CodigoTextField.getText().compareTo("82380")==0 && SenhaTextField.getText().compareTo("82380")==0){
-            this.dispatchEvent(e);
+            new MainJFrame().setVisible(true);
+            this.dispose();
         }
-    }    
+    }//GEN-LAST:event_OKJButtonActionPerformed
 
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginJFrame().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField CodigoTextField;
     private javax.swing.JButton OKJButton;
     private javax.swing.JTextField SenhaTextField;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private AWTEvent e;
+    // End of variables declaration//GEN-END:variables
 }
